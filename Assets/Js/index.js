0c031c96dab6ab29fbacc6e27f43715b731194b9
Ujjwal Sharma <imshanusharma@users.noreply.github.com>
2020-11-21 22:38:43 +0530

    first commit

---
+++ Assets/Js/index.js
@@ -0,0 +1,144 @@
+const linkObject = [
+    {
+        1 : [
+            '16qp2rLfG_08M58u-oW1O7ztCVYF7yHfU',
+            '1LNlOnlkzRdD5pqgpHLSM5tseWESh9LWw',
+            '125-HkAPxQHM9d1R-2R28TaLfp37S2qud',
+            '1QBXuNCqSZ8htDgYKs8AneoRvWGeLCIUR'
+        ],
+        2 : [
+            '1F4D7oGSYZGGq1oBfwmhctOh3kGZ7CjQy',
+            '13ist09wcqx1NrIPNKjbwkqC61dPru1lN',
+            '1Ml6YokT-dS23oi6VQIayth_h2BtMScHP',
+            '1J0YaB2PqfcOATxPjQbQUrBpAKsd3N6ym'
+        ],
+        3 : [
+            '16qp2rLfG_08M58u-oW1O7ztCVYF7yHfU',
+            '1LNlOnlkzRdD5pqgpHLSM5tseWESh9LWw',
+            '125-HkAPxQHM9d1R-2R28TaLfp37S2qud',
+            '1QBXuNCqSZ8htDgYKs8AneoRvWGeLCIUR'
+        ],
+        4 : [
+            '1F4D7oGSYZGGq1oBfwmhctOh3kGZ7CjQy',
+            '13ist09wcqx1NrIPNKjbwkqC61dPru1lN',
+            '1Ml6YokT-dS23oi6VQIayth_h2BtMScHP',
+            '1J0YaB2PqfcOATxPjQbQUrBpAKsd3N6ym'
+        ],
+        5 : [
+            '16qp2rLfG_08M58u-oW1O7ztCVYF7yHfU',
+            '1LNlOnlkzRdD5pqgpHLSM5tseWESh9LWw',
+            '125-HkAPxQHM9d1R-2R28TaLfp37S2qud',
+            '1QBXuNCqSZ8htDgYKs8AneoRvWGeLCIUR'
+        ],
+        6 : [
+            '1F4D7oGSYZGGq1oBfwmhctOh3kGZ7CjQy',
+            '13ist09wcqx1NrIPNKjbwkqC61dPru1lN',
+            '1Ml6YokT-dS23oi6VQIayth_h2BtMScHP',
+            '1J0YaB2PqfcOATxPjQbQUrBpAKsd3N6ym'
+        ],
+        7 : [
+            '16qp2rLfG_08M58u-oW1O7ztCVYF7yHfU',
+            '1LNlOnlkzRdD5pqgpHLSM5tseWESh9LWw',
+            '125-HkAPxQHM9d1R-2R28TaLfp37S2qud',
+            '1QBXuNCqSZ8htDgYKs8AneoRvWGeLCIUR'
+        ],
+        8 : [
+            '1F4D7oGSYZGGq1oBfwmhctOh3kGZ7CjQy',
+            '13ist09wcqx1NrIPNKjbwkqC61dPru1lN',
+            '1Ml6YokT-dS23oi6VQIayth_h2BtMScHP',
+            '1J0YaB2PqfcOATxPjQbQUrBpAKsd3N6ym'
+        ]
+    },
+    {
+        1 : [
+            '16qp2rLfG_08M58u-oW1O7ztCVYF7yHfU',
+            '1LNlOnlkzRdD5pqgpHLSM5tseWESh9LWw',
+            '125-HkAPxQHM9d1R-2R28TaLfp37S2qud',
+            '1QBXuNCqSZ8htDgYKs8AneoRvWGeLCIUR'
+        ],
+        2 : [
+            '1F4D7oGSYZGGq1oBfwmhctOh3kGZ7CjQy',
+            '13ist09wcqx1NrIPNKjbwkqC61dPru1lN',
+            '1Ml6YokT-dS23oi6VQIayth_h2BtMScHP',
+            '1J0YaB2PqfcOATxPjQbQUrBpAKsd3N6ym'
+        ],
+        3 : [
+            '16qp2rLfG_08M58u-oW1O7ztCVYF7yHfU',
+            '1LNlOnlkzRdD5pqgpHLSM5tseWESh9LWw',
+            '125-HkAPxQHM9d1R-2R28TaLfp37S2qud',
+            '1QBXuNCqSZ8htDgYKs8AneoRvWGeLCIUR'
+        ],
+        4 : [
+            '1F4D7oGSYZGGq1oBfwmhctOh3kGZ7CjQy',
+            '13ist09wcqx1NrIPNKjbwkqC61dPru1lN',
+            '1Ml6YokT-dS23oi6VQIayth_h2BtMScHP',
+            '1J0YaB2PqfcOATxPjQbQUrBpAKsd3N6ym'
+        ],
+        5 : [
+            '16qp2rLfG_08M58u-oW1O7ztCVYF7yHfU',
+            '1LNlOnlkzRdD5pqgpHLSM5tseWESh9LWw',
+            '125-HkAPxQHM9d1R-2R28TaLfp37S2qud',
+            '1QBXuNCqSZ8htDgYKs8AneoRvWGeLCIUR'
+        ],
+        6 : [
+            '1F4D7oGSYZGGq1oBfwmhctOh3kGZ7CjQy',
+            '13ist09wcqx1NrIPNKjbwkqC61dPru1lN',
+            '1Ml6YokT-dS23oi6VQIayth_h2BtMScHP',
+            '1J0YaB2PqfcOATxPjQbQUrBpAKsd3N6ym'
+        ],
+        7 : [
+            '16qp2rLfG_08M58u-oW1O7ztCVYF7yHfU',
+            '1LNlOnlkzRdD5pqgpHLSM5tseWESh9LWw',
+            '125-HkAPxQHM9d1R-2R28TaLfp37S2qud',
+            '1QBXuNCqSZ8htDgYKs8AneoRvWGeLCIUR'
+        ],
+        8 : [
+            '1F4D7oGSYZGGq1oBfwmhctOh3kGZ7CjQy',
+            '13ist09wcqx1NrIPNKjbwkqC61dPru1lN',
+            '1Ml6YokT-dS23oi6VQIayth_h2BtMScHP',
+            '1J0YaB2PqfcOATxPjQbQUrBpAKsd3N6ym'
+        ]
+    }
+]
+
+var link = "https://drive.google.com/file/d/"+ linkObject[0][7][2] + "/view?usp=sharing";
+
+var course = document.getElementById('course').options;
+var semester = document.getElementById('semester').options;
+var subjects = document.getElementById('subject').options;
+document.getElementById('a').style.display = 'none';
+
+document.getElementById('submit').addEventListener('click',()=>{
+    var courseIndex = document.getElementById('course').selectedIndex;
+    var semesterIndex = document.getElementById('semester').selectedIndex;
+    var subjectIndex = document.getElementById('subject').selectedIndex;
+    var i1 = course[courseIndex].value;
+    var i2 = semester[semesterIndex].value;
+    var i3 = subjects[subjectIndex].value;
+    link = "https://drive.google.com/file/d/" + linkObject[i1][i2][i3] +"/view?usp=sharing" ;
+    var err = false;
+    if(courseIndex == 0){
+        document.getElementById('a').style.display = 'none';
+        document.getElementById('h1').style.display = 'block';
+        document.getElementById('h1').innerHTML = "Please select Course";
+        err = true;
+    }
+    else if(semesterIndex == 0){
+        document.getElementById('a').style.display = 'none';
+        document.getElementById('h1').style.display = 'block';
+        document.getElementById('h1').innerHTML = "Please select Semester";
+        err = true;
+    }
+    else if(subjectIndex == 0){
+        document.getElementById('a').style.display = 'none';
+        document.getElementById('h1').style.display = 'block';
+        document.getElementById('h1').innerHTML = "Please select subject";
+        err = true;
+    }
+    if(err == false){
+        document.getElementById('h1').style.display = 'none';
+        document.getElementById('a').style.display = 'inline';
+        document.getElementById('a').setAttribute('href',link);
+    }
+    
+});
